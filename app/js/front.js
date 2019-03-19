cef82e3b8efae0c0b9808428f562934e1bafaaca
Lucas Merat <lucas.merat@gmail.com>
2019-03-19 09:06:27 -0500

    Fixed bug where only first question would be added for every one

--- app/js/front.js
+++ app/js/front.js
@@ -17,15 +17,15 @@ document.addEventListener('DOMContentLoaded', function() {
         firstname: $('#first_name').val().trim(),
         scores: {
             q1: $('#q1').val(),
-            q2: $('#q1').val(),
-            q3: $('#q1').val(),
-            q4: $('#q1').val(),
-            q5: $('#q1').val(),
-            q6: $('#q1').val(),
-            q7: $('#q1').val(),
-            q8: $('#q1').val(),
-            q9: $('#q1').val(),
-            q10: $('#q1').val()
+            q2: $('#q2').val(),
+            q3: $('#q3').val(),
+            q4: $('#q4').val(),
+            q5: $('#q5').val(),
+            q6: $('#q6').val(),
+            q7: $('#q7').val(),
+            q8: $('#q8').val(),
+            q9: $('#q9').val(),
+            q10: $('#q10').val()
     }
     };
 
@@ -38,6 +38,7 @@ document.addEventListener('DOMContentLoaded', function() {
         //Triggers modal from materialize
        $('.modal').modal();
        $('.modal').modal('open'); 
+       $(".modal-close").on("click", () => location.reload())
 
        //Adds winning cast member to modal
        let name = data.name;
--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,53 +1,53 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const friends = require('../data/friends')
-const bodyParser = require('body-parser')
+const friends = require("../data/friends");
+const bodyParser = require("body-parser");
 
-const urlEncodedParser = bodyParser.urlencoded({ extended: true })
+const urlEncodedParser = bodyParser.urlencoded({ extended: true });
 
-
-router.get('/api/cast', (req,res)=>{
-    console.log('Get some json!');
-    res.json(friends);
+router.get("/api/cast", (req, res) => {
+  console.log("Get some json!");
+  res.json(friends);
 });
 
-router.post('/survey', urlEncodedParser, (req,res)=>{
-    let request = req.body;
-    //Populating filled out user scores from request to iterable array
-    let userScores = request.scores; 
-    let userScoreArr = [];
-    for(let score in userScores){
-        userScoreArr.push(Number(userScores[score]))
+router.post("/survey", urlEncodedParser, (req, res) => {
+  let request = req.body;
+  //Populating filled out user scores from request to iterable array
+  let userScores = request.scores;
+  let userScoreArr = [];
+  console.log(request)
+  for (let score in userScores) {
+      console.log(userScores.q2)
+    userScoreArr.push(Number(userScores[score]));
+  }
+
+  //Initializing arr of differences between each friend and variable to hold the index of that friend, lastly, a variable to store the lowest difference or selected friend
+  let friendDiffs = [];
+  let friendIndex = 0;
+  let lowestDiffIndex = 0;
+
+  //Loops through all 4 friends to find a closest match, comparing all scores to an aggregate and pushing the lowest to the winning index
+  while (friendIndex < 5) {
+    let totalDiff = 0;
+    for (let i = 0; i < userScoreArr.length; i++) {
+      let diff = Math.abs(friends[friendIndex].scores[i] - userScoreArr[i]);
+      totalDiff += diff;
     }
+    friendDiffs.push(totalDiff);
 
-    //Initializing arr of differences between each friend and variable to hold the index of that friend, lastly, a variable to store the lowest difference or selected friend
-    let friendDiffs = [];
-    let friendIndex = 0;
-    let lowestDiffIndex = 0;
-
-    //Loops through all 4 friends to find a closest match, comparing all scores to an aggregate and pushing the lowest to the winning index
-    while(friendIndex < 5){
-        let totalDiff = 0;
-        for(let i=0;i<userScoreArr.length;i++){
-            let diff = Math.abs(friends[friendIndex].scores[i] - userScoreArr[i])
-            totalDiff+=diff
-        }
-        friendDiffs.push(totalDiff);
-        console.log(friendDiffs[friendIndex])
-        console.log(friendDiffs[friendIndex-1])
-        if(friendDiffs[friendIndex] <= friendDiffs[lowestDiffIndex]){
-            console.log(lowestDiffIndex)
-            lowestDiffIndex = friendIndex;
-            console.log("Newest low diff index is " + lowestDiffIndex)
-        }
-        friendIndex++;
+    if (friendDiffs[friendIndex] <= friendDiffs[lowestDiffIndex]) {
+      lowestDiffIndex = friendIndex;
+      console.log("Newest low diff index is " + lowestDiffIndex);
     }
-    console.log(friendDiffs)
+    friendIndex++;
+  }
+  console.log(`Your scores were: ${userScoreArr}`)
+  console.log(`The fab 5's scores were: ${friendDiffs}`);
 
-    console.log(`The selected friend is: ${friends[lowestDiffIndex].name}`)
+  console.log(`The selected friend is: ${friends[lowestDiffIndex].name}`);
 
-    //Sending best matched friend to front end
-    res.json(friends[lowestDiffIndex]);
+  //Sending best matched friend to front end
+  res.json(friends[lowestDiffIndex]);
 });
 
 module.exports = router;
